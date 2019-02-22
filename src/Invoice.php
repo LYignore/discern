bcc16b075fde7ee11948fb8a109487ab97798abc
liuyang <1585296643@qq.com>
2019-02-22 20:03:11 +0800

    a test for uploadInvoice

--- src/Invoice.php
+++ src/Invoice.php
@@ -6,15 +6,22 @@ use Lyignore\Discern\Support\Config;
 class Invoice implements \Lyignore\Discern\Contracts\InvoiceInterface{
     private $config;
 
+    private $messager;
+
+    private $file;
+
     public function __construct(array $config){
         $this->config = new Config($config);
+        $this->messager = new Messenger($this->config);
     }
 
     /*
      * 上传发票
      */
-    public function uploadInvoice(string $path){
+    public function uploadInvoice(string $path, $type = 'invoice'){
         //实例化上传对象
+        $this->file = new File($path);
+        return $this->messager->invoiceDiscern($this->file, $type);
     }
 
     /*
